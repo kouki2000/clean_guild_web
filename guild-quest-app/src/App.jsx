b7fb3c0a79501a295686bf747a1930d5b6ac58e4
kouki2000 <kouki-tus@outlook.jp>
2025-10-04 23:29:16 +0900

    feat:フッター生成

--- guild-quest-app/src/App.jsx
+++ guild-quest-app/src/App.jsx
@@ -1,11 +1,13 @@
 import HeroSection from './components/home/HeroSection'
 import FeaturesSection from './components/home/FeaturesSection'
+import Footer from './components/common/Footer'
 
 function App() {
   return (
     <div className="w-full">
       <HeroSection />
       <FeaturesSection />
+      <Footer />
     </div>
   )
 }
